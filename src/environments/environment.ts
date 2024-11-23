64be1c6eadcc39c3af64d39509b6f8468629d820
HamadiBlagui <90401108+HamadiBlagui@users.noreply.github.com>
2024-11-23 11:33:45 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
    backUrl: "http://192.168.56.3:8082/SpringMVC"
 };
 
